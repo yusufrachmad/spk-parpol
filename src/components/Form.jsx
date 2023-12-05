6223120dc2b6bfcdb559f6aaffe5ea0114015efa
charmingnim <100217101+yusufrachmad@users.noreply.github.com>
2023-12-05 23:02:37 +0700

    form to choose profile

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,60 @@
 import React, { useState, useEffect } from "react";
+import { supabase } from "../utils/supabaseClient";
+import FormSlider from "./FormSlider";
+import { Card, Col, Container, Row } from "react-bootstrap";
 
-const Form = (props) => {
+const Form = ({ onChange }) => {
+  const [data, setData] = useState([]);
+  const [subCriteria, setSubCriteria] = useState([]);
+  const [lastSubCriteria, setLastSubCriteria] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data: subcriteria, error } = await supabase
+          .from("subkriteria")
+          .select("subkriteria, kriteria(kode_kriteria)");
+
+        if (data) {
+          setData(data);
+        }
+
+        if (subcriteria) {
+          setSubCriteria(subcriteria.slice(0, 5));
+          setLastSubCriteria(subcriteria.slice(5, 10));
+        }
+      } catch (error) {
+        console.error("Error fetching data: ", error.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
-      <div>AKU ADALAH FORM</div>
-      <div></div>
+      <Container>
+        <Row>
+          <Col>
+            {subCriteria.map((item, index) => (
+              <div className="mb-4" key={index}>
+                <Card>
+                  <FormSlider criteria={item.subkriteria} onChange={onChange} />
+                </Card>
+              </div>
+            ))}
+          </Col>
+          <Col>
+            {lastSubCriteria.map((item, index) => (
+              <div className="mb-4" key={index}>
+                <Card>
+                  <FormSlider criteria={item.subkriteria} onChange={onChange} />
+                </Card>
+              </div>
+            ))}
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
--- src/components/pages/System.jsx
+++ src/components/pages/System.jsx
@@ -1,17 +1,92 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useRef } from "react";
 import Form from "../Form.jsx";
 import Result from "../Result.jsx";
+import "../../style/System.css";
+import { Container } from "react-bootstrap";
+import Button from "@mui/material/Button";
+import { topsis } from "../../utils/topsis";
 
 const System = () => {
+  const [sliderValues, setSliderValues] = useState([]);
+  const [resultData, setResultData] = useState([]);
+  const resultRef = useRef(null);
+
+  const handleSliderChange = (criteria, value) => {
+    setSliderValues((prevValues) => ({
+      ...prevValues,
+      [criteria]: value,
+    }));
+  };
+
+  const handleValueWarning = (target) => {
+    if (
+      target[0] + target[1] === 10 ||
+      target[0] + target[1] === 1 ||
+      target[5] + target[6] === 10 ||
+      target[5] + target[6] === 1 ||
+      target[8] + target[9] === 10 ||
+      target[8] + target[9] === 1
+    ) {
+      alert("Nilai tidak boleh 0 atau 10");
+      return true;
+    }
+
+    return false;
+  };
+
+  const handleSubmit = async () => {
+    const target = Object.values(sliderValues);
+    if (handleValueWarning(target)) {
+      return;
+    }
+
+    try {
+      const data = await topsis(target);
+
+      if (data) {
+        setResultData(data);
+        resultRef.current.scrollIntoView({ behavior: "smooth" });
+      }
+    } catch (error) {
+      console.error("Error calculating data: ", error.message);
+    }
+  };
+
   return (
-    <>
-      <div>
-        <Form />
-      </div>
-      <div>
-        <Result />
-      </div>
-    </>
+    <div className="content-wrapper">
+      <Container>
+        <div className="d-flex justify-content-center align-items-center mb-4">
+          <h1
+            style={{ fontFamily: "Poppins", fontWeight: 700, fontSize: "30pt" }}
+          >
+            Tentukan Pertimbanganmu
+          </h1>
+        </div>
+        <Form onChange={handleSliderChange} />
+        <div className="d-flex justify-content-center align-items-center mt-1 mb-3">
+          <Button variant="outlined" size="medium" onClick={handleSubmit}>
+            Submit
+          </Button>
+        </div>
+      </Container>
+      <div ref={resultRef}></div>
+      {resultData.length > 0 && (
+        <Container style={{ marginTop: "144px" }}>
+          <div className="d-flex justify-content-center align-items-center mb-4">
+            <h1
+              style={{
+                fontFamily: "Poppins",
+                fontWeight: 700,
+                fontSize: "30pt",
+              }}
+            >
+              Hasil Perhitungan
+            </h1>
+          </div>
+          <Result result={resultData} />
+        </Container>
+      )}
+    </div>
   );
 };
 
