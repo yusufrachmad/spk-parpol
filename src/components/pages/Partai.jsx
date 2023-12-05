39ea6ba34aee2740840dfc0d277ee3d25e3421ad
charmingnim <100217101+yusufrachmad@users.noreply.github.com>
2023-12-05 23:03:45 +0700

    change partai page layout

--- src/components/pages/Partai.jsx
+++ src/components/pages/Partai.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Container, Row, Col, Image, Card } from "react-bootstrap";
-import { supabase } from "../../supabaseClient.js";
+import { supabase } from "../../utils/supabaseClient.js";
 import "../../style/Partai.css";
 
 const Partai = () => {
@@ -33,30 +33,58 @@ const Partai = () => {
   }, []);
 
   return (
-    <Container className="mt-5 d-flex align-items-center">
+    <div
+      className="align-items-center"
+      style={{ height: "100vh", paddingTop: "160px" }}
+      id="partai"
+    >
+      <div className="d-flex justify-content-center mb-4 text-center">
+        <h1
+          style={{
+            fontFamily: "Poppins",
+            fontWeight: 700,
+          }}
+          className="text-responsive"
+        >
+          Daftar Partai Peserta Pemilu 2024
+        </h1>
+      </div>
       {fetchError && <p>{fetchError}</p>}
-      <Row>
-        {dataPartai.map((item) => (
-          <Col
-            md={4}
-            sm={12}
-            xs={16}
-            className="mb-5 offset-md-0 offset-sm-3 offset-xs-3"
-          >
-            <Card className="partai-logo">
-              <div className="image-wrapper">
-                <Image
-                  key={item.id_partai}
-                  src={`/src/assets/partai/${item.singkatan}.png`}
-                  alt={item.singkatan}
-                  className="images"
-                />
-              </div>
-            </Card>
-          </Col>
-        ))}
-      </Row>
-    </Container>
+      <Container className="d-flex align-items-center justify-content-center">
+        <Card style={{ border: "none" }}>
+          <Row>
+            {dataPartai.map((item, index) => (
+              <Col lg={2} md={3} xs={4}>
+                <div className="d-flex justify-content-center" key={index}>
+                  <Card className="partai-logo" style={{ border: "none" }}>
+                    <Card.Body className="image-wrapper">
+                      <Card.Title className="align-items-center">
+                        <Image
+                          src={`/src/assets/partai/${item.singkatan}.png`}
+                          alt={item.singkatan}
+                          className="logo-partai"
+                        />
+                      </Card.Title>
+                    </Card.Body>
+                  </Card>
+                </div>
+              </Col>
+            ))}
+          </Row>
+        </Card>
+      </Container>
+      <Container className="d-flex justify-content-center align-items-center">
+        <div
+          className="d-flex justify-content-center align-items-center text-center"
+          style={{ width: "800px", paddingTop: "50px" }}
+        >
+          <h4 style={{ fontFamily: "Open Sans" }}>
+            18 partai bersaing pada Pemilu 2024 untuk memperebutkan jatah kursi
+            pada lembaga Dewan Perwakilan Rakyat
+          </h4>
+        </div>
+      </Container>
+    </div>
   );
 };
 
