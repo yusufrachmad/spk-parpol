cfc799a6bf0d3456afa43c62befd0a38ab5b1386
charmingnim <100217101+yusufrachmad@users.noreply.github.com>
2023-12-05 23:00:47 +0700

    add-footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import NavigationBar from "./components/NavigationBar.jsx";
 import About from "./components/pages/About.jsx";
 import Home from "./components/pages/Home.jsx";
 import System from "./components/pages/System.jsx";
+import Footer from "./components/Footer.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
@@ -16,14 +17,12 @@ function App() {
             <Route index element={<Home />}></Route>
             <Route path="/home" element={<Home />}></Route>
             <Route path="/about" element={<About />}></Route>
-            <Route path="/helpme" element={<System />}></Route>
+            <Route path="/bantu-aku" element={<System />}></Route>
           </Route>
         </Routes>
       </Router>
 
-      {/* <div className="footer">
-        <Footer />
-      </div> */}
+      <Footer />
     </div>
   );
 }
