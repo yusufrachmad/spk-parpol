4d994d9f2bd0a40c6df2a6b8a28d2e65b202483c
charmingnim <100217101+yusufrachmad@users.noreply.github.com>
2023-12-05 23:01:27 +0700

    modify-navbar

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,40 +1,67 @@
 import { React, useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { Container, Nav, Navbar, NavbarBrand } from "react-bootstrap";
 import lightLogo from "../assets/logo2.png";
 import darkLogo from "../assets/logo1.png";
 import "../style/Navbar.css";
 
 const NavigationBar = () => {
+  const routeLoc = useLocation();
   const [timeToChange, setTimeToChange] = useState(false);
-  const [imageLogo, setImageLogo] = useState(darkLogo);
+  const [imageLogo, setImageLogo] = useState(lightLogo);
+  const [expanded, setExpanded] = useState(false);
+
+  const toggleNavbar = () => {
+    setExpanded(!expanded);
+  };
 
   useEffect(() => {
-    const handleScroll = () => {
-      const verticalScroll = window.scrollY;
+    const isHomepage =
+      routeLoc.pathname === "/" || routeLoc.pathname === "/home";
 
-      if (verticalScroll > 100) {
-        setTimeToChange(true);
-      } else {
-        setTimeToChange(false);
-      }
-    };
+    if (isHomepage) {
+      const handleScroll = () => {
+        const verticalScroll = window.scrollY;
 
-    window.addEventListener("scroll", handleScroll);
+        if (verticalScroll > 100) {
+          setTimeToChange(true);
+          setImageLogo(lightLogo);
+        } else {
+          setTimeToChange(false);
+          setImageLogo(darkLogo);
+        }
+      };
 
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+      window.addEventListener("scroll", handleScroll);
 
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      };
+    }
+  }, [routeLoc.pathname]);
+
+  // Change logo based on whether it's the homepage or other pages
+  const isHomepage = location.pathname === "/" || location.pathname === "/home";
   useEffect(() => {
-    setImageLogo(timeToChange ? lightLogo : darkLogo);
-  }, [timeToChange]);
+    setImageLogo(isHomepage ? darkLogo : lightLogo);
+  }, [isHomepage]);
 
   const navStyle = {
-    backgroundColor: timeToChange ? "#ffffff" : "transparent",
-    borderBottom: timeToChange ? "0.5px solid #ffffff" : "0px",
-    boxShadow: timeToChange ? "0px 2px 4px rgba(0, 0, 0, 0.1)" : "0px 0px 0px",
+    backgroundColor: isHomepage
+      ? timeToChange
+        ? "#ffffff"
+        : "transparent"
+      : "#ffffff",
+    borderBottom: isHomepage
+      ? timeToChange
+        ? "0.5px solid #ffffff"
+        : "0px"
+      : "0.5px solid #ffffff",
+    boxShadow: isHomepage
+      ? timeToChange
+        ? "0px 2px 4px rgba(0, 0, 0, 0.1)"
+        : "0px 0px 0px"
+      : "0px 2px 4px rgba(0, 0, 0, 0.1)",
     transition: "background-color 0.4s ease-in-out",
     position: "fixed",
     top: 0,
@@ -43,34 +70,77 @@ const NavigationBar = () => {
   };
 
   const navText = {
-    color: timeToChange ? "#000000" : "#ffffff",
+    color: isHomepage ? (timeToChange ? "#000000" : "#ffffff") : "#000000",
   };
 
   const navbarClass = timeToChange ? "dark" : "light";
 
+  const scrollToSection = (id) => {
+    const section = document.getElementById(id);
+    if (isHomepage) {
+      if (section) {
+        section.scrollIntoView({ behavior: "smooth" });
+      }
+      setExpanded(false);
+    } else {
+      setTimeout(() => {
+        window.location.href = "/";
+        setTimeout(() => {
+          if (section) {
+            section.scrollIntoView({ behavior: "smooth" });
+          } else {
+            console.log("bisa gila");
+          }
+        }, 100);
+      }, 100);
+    }
+  };
+
   return (
     <div>
-      <Navbar style={navStyle}>
+      <Navbar expand="lg" style={navStyle} expanded={expanded}>
         <Container>
           <NavbarBrand className="logo">
             <Link to="/">
               <img
                 src={imageLogo}
                 alt="bantu-memilih"
-                className={`logo-img ${
-                  navbarClass === "dark" ? "dark" : "light"
-                }`}
+                className={`logo-img ${navbarClass}`}
               ></img>
             </Link>
           </NavbarBrand>
-          <Nav className="nav-text">
-            <Nav.Link style={navText} href="/home">
-              Home
-            </Nav.Link>
-            <Nav.Link style={navText} href="/about">
-              About
-            </Nav.Link>
-          </Nav>
+          <div className="justify-content-end">
+            <Navbar.Toggle
+              aria-controls="basic-navbar-nav"
+              style={{ color: "#fffffff" }}
+              onClick={toggleNavbar}
+            />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="nav-text">
+                <Nav.Link
+                  style={navText}
+                  onClick={() => scrollToSection("home")}
+                >
+                  Home
+                </Nav.Link>
+                <Nav.Link
+                  style={navText}
+                  onClick={() => scrollToSection("partai")}
+                >
+                  Partai
+                </Nav.Link>
+                <Nav.Link
+                  style={navText}
+                  onClick={() => scrollToSection("kriteria")}
+                >
+                  Kriteria
+                </Nav.Link>
+                <Nav.Link style={navText} href="/about">
+                  Tentang
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </div>
         </Container>
       </Navbar>
     </div>
