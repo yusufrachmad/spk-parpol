94643d57f06331a9f7a9c23e068049792b100f28
charmingnim <100217101+yusufrachmad@users.noreply.github.com>
2023-12-18 23:50:03 +0700

    make nilai profil as component

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -3,45 +3,20 @@ import { Row, Col, Container, Card, Image } from "react-bootstrap";
 import WinnerParty from "./WinnerParty";
 import ListOtherResult from "./ListOtherResult";
 import FeedbackSlider from "./FeedbackSlider";
+import NilaiProfil from "./NilaiProfil";
 
-const Result = ({ result, target }) => {
+const Result = ({ result, target, profile }) => {
   return (
     <>
       <Container className="justify-content-center align-items-center mb-5">
         <Row>
           <Col lg={6} md={12} xs={16} className="mb-3">
-            <Card className="mb-4 d-flex">
-              <div className="d-flex justify-content-center mb-4 mt-3">
-                <h1
-                  style={{
-                    fontFamily: "Poppins",
-                    fontWeight: 600,
-                    fontSize: "20pt",
-                  }}
-                >
-                  Nilai Profil
-                </h1>
-              </div>
-              <div className="d-flex justify-content-center">
-                <Row>
-                  {Object.entries(target).map(([key, value]) => (
-                    <Col
-                      className="d-flex justify-content-center"
-                      lg={2}
-                      md={3}
-                      xs={4}
-                    >
-                      <p style={{ fontFamily: "Poppins" }} key={key}>
-                        {key}: {value}
-                      </p>
-                    </Col>
-                  ))}
-                </Row>
-              </div>
-            </Card>
             <Card className="mb-4">
               <WinnerParty data={result} />
             </Card>
+            <div className="mb-4">
+              <NilaiProfil profile={profile} />
+            </div>
             <div>
               <FeedbackSlider target={target} result={result} />
             </div>
