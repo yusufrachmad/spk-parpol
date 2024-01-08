0c98599b6a130f93b2993b8de950cda6d1cc0792
charmingnim <100217101+yusufrachmad@users.noreply.github.com>
2024-01-08 14:33:18 +0700

    token for admin

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,26 @@ import About from "./components/pages/About.jsx";
 import Home from "./components/pages/Home.jsx";
 import System from "./components/pages/System.jsx";
 import Footer from "./components/Footer.jsx";
+import Login from "./components/pages/Login.jsx";
+import AdminPage from "./components/pages/AdminPage.jsx";
+import ErrorPage from "./components/pages/ErrorPage.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [token, setToken] = useState(false);
+
+  if (token) {
+    sessionStorage.setItem("token", JSON.stringify(token));
+  }
+
+  useEffect(() => {
+    if (sessionStorage.getItem("token")) {
+      let data = JSON.parse(sessionStorage.getItem("token"));
+      setToken(data);
+    }
+  }, []);
+
   return (
     <div className="App">
       <Router>
@@ -18,6 +35,15 @@ function App() {
             <Route path="/home" element={<Home />}></Route>
             <Route path="/about" element={<About />}></Route>
             <Route path="/bantu-aku" element={<System />}></Route>
+            <Route
+              path="/login"
+              element={<Login setToken={setToken} />}
+            ></Route>
+            {token ? (
+              <Route path="/adminpage/*" element={<AdminPage />} />
+            ) : (
+              <Route path="/adminpage/*" element={<ErrorPage />} />
+            )}
           </Route>
         </Routes>
       </Router>
