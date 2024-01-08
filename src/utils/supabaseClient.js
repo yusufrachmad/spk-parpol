a9ef9f38190d75664c71635acedc9e96d8c4cd14
charmingnim <100217101+yusufrachmad@users.noreply.github.com>
2024-01-08 14:34:29 +0700

    client untuk admin

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -2,5 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+// const supabaseServiceKey = import.meta.env.VITE_SUPABASE_SERVICE_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, { db: { schema: 'spkparpol' } });
+// export const supabaseService = createClient(supabaseUrl, supabaseServiceKey, { db: { schema: 'spkparpol' } });
